3736ae6eed16d75af0a61f703bea109414b203e5
mranxiety <mcleish.john+mranxiety@gmail.com>
2025-03-11 23:38:44 +1100

    Update bannerScript.js
    Attempted to parse the RSS feed version of my profile. Bluesky blocks because of CORS (Cross Origin Resource Sharing)
    
    So it looks like I'll have to figure out the API to access my posts. Cool, time to learn typescript.

--- bannerScript.js
+++ bannerScript.js
@@ -1,3 +1,27 @@
 // Links to implementing RSS in JS: https://dev.to/geekgalgroks/building-an-rss-reader-in-javascript-1ep0
 // https://css-tricks.com/how-to-fetch-and-parse-rss-feeds-in-javascript/
 // https://docs.bsky.app/
+
+const RSS_URL = "https://bsky.app/profile/did:plc:uexvzvxiyl5ntq7h46vt5vix/rss";
+
+fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    console.log(data);
+    const items = data.querySelectorAll("item");
+    let html = ``;
+    items.forEach(el => {
+      html += `
+        <article>
+          <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
+          <h2>
+            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+              ${el.querySelector("title").innerHTML}
+            </a>
+          </h2>
+        </article>
+      `;
+    });
+    document.body.insertAdjacentHTML("beforeend", html);
+  });
