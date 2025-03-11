84f8f0d7f8d8d1f887684cd1edbae70b3468ea1e
mranxiety <mcleish.john+mranxiety@gmail.com>
2025-03-11 23:09:20 +1100

    Initial setup
    Migrated over my code for my current banner into the repo.
    
    The only thing I left out was the hardcoded posts in <p> tags because the plan is to dynamically place those with either an API call or by parsing an RSS feed. I'm still thinking on that.

---
+++ bannerScript.js
@@ -0,0 +1,3 @@
+// Links to implementing RSS in JS: https://dev.to/geekgalgroks/building-an-rss-reader-in-javascript-1ep0
+// https://css-tricks.com/how-to-fetch-and-parse-rss-feeds-in-javascript/
+// https://docs.bsky.app/
